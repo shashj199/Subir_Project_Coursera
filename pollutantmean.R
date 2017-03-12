83c268be221315db72f4cdf3b55cab41f283b5a8
shashj199 <shashank.jatav@elucidata.io>
2017-03-12 19:17:00 +0530

    Identation and Brackecting

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,5 +1,4 @@
-pollutantmean <-function(directory,pollutant, id=1:332)
-{
-	x<- (mean(pollutant,na.rm = TRUE)
-print(x)
+pollutantmean <-function(directory,pollutant, id=1:332){
+  x<- (mean(pollutant,na.rm = TRUE))
+  print(x)
 }
