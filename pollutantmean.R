37a46be248a46c2838796dbf0d4b05954e500041
shashj199 <shashank.jatav@elucidata.io>
2017-03-12 19:36:36 +0530

    Read files

--- pollutantmean.R
+++ pollutantmean.R
@@ -5,7 +5,7 @@ pollutantmean <-function(directory,pollutant, id=1:332){
     filenames[count]<-paste("00", i, ".csv", sep = "")
     count<-count+1
   }
-  
+  data_files<-lapply(filenames, function(x){read.csv(file = file.path("SpecData",x),stringsAsFactors = FALSE)})
   
   x<- (mean(pollutant,na.rm = TRUE))
   print(x)
