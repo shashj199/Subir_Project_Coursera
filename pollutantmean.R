8d855c10b05c067d877fbe90ab2aa5472ad23818
shashj199 <shashank.jatav@elucidata.io>
2017-03-12 20:10:49 +0530

    Calculating mean

--- pollutantmean.R
+++ pollutantmean.R
@@ -7,6 +7,12 @@ pollutantmean <-function(directory,pollutant, id=1:332){
   }
   data_files<-lapply(filenames, function(x){read.csv(file = file.path("SpecData",x),stringsAsFactors = FALSE)})
   
+  Mean_PollutantbyID<-lapply(data_files, function(x){mean(x$pollutant,na.rm = TRUE)})
+  for(i in 1:length(data_files)){
+    data_mean<-data_files[[i]][pollutant]
+    print(length(data_mean))
+    Mean_PollutantbyID[i]<-mean(data_mean, na.rm = TRUE)
+  }
   x<- (mean(pollutant,na.rm = TRUE))
   print(x)
 }
