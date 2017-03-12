d48bb8d0d350e650c8c76004fd4b0f8280dc6869
shashj199 <shashank.jatav@elucidata.io>
2017-03-12 19:30:19 +0530

    Make a vector of filenames to be read

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,4 +1,12 @@
 pollutantmean <-function(directory,pollutant, id=1:332){
+  filenames<-c()
+  count<-1
+  for (i in id) {
+    filenames[count]<-paste("00", i, ".csv", sep = "")
+    count<-count+1
+  }
+  
+  
   x<- (mean(pollutant,na.rm = TRUE))
   print(x)
 }
